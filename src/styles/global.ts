858cef858a6fcac91332ec39550bd29b0ecc4fae
Tiago Vieira <tiagosvieira10@gmail.com>
2023-09-20 17:40:10 -0300

    config incial feita

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors["base-background"]};
+    color: ${({theme}) => theme.colors["base-text"]};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-weight: 400;
+    font-size: ${({ theme }) => theme.textSizes["text-regular-m"]}
+  }
+`
