27295adb816fb89c5b007a7f25105bd47c944338
Tiago Vieira <tiagosvieira10@gmail.com>
2023-09-25 13:13:20 -0300

    carrinho - endereço header finalizado

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,5 +1,6 @@
+import { MapPinLine } from "@phosphor-icons/react";
 import { Header } from "../../components/Header";
-import { CarrinhoContainer, CarrinhoMain, CoffeSelect, CompletePedido, EnderecoContainer, PagamentoContainer } from "./styles";
+import { CarrinhoContainer, CarrinhoMain, CoffeSelect, CompletePedido, EnderecoContainer, HeaderContainerCards, PagamentoContainer } from "./styles";
 
 export function CarrinhoPage() {
   return (
@@ -8,16 +9,26 @@ export function CarrinhoPage() {
 
       <CarrinhoMain>
         <CompletePedido>
+          <h4>Complete seu pedido</h4>
           <EnderecoContainer>
 
+            <HeaderContainerCards>
+              <MapPinLine color="#C47F17" size={22}/>
+            <div>
+              <p>Endereço de Entrega</p>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+            </HeaderContainerCards>
+
           </EnderecoContainer>
+
           <PagamentoContainer>
 
           </PagamentoContainer>
         </CompletePedido>
 
         <CoffeSelect>
-          
+          <h4>Cafés selecionados</h4>
         </CoffeSelect>
       </CarrinhoMain>
       
--- src/pages/Carrinho/styles.ts
+++ src/pages/Carrinho/styles.ts
@@ -4,14 +4,47 @@ export const CarrinhoContainer = styled.main`
 
 `
 export const CarrinhoMain = styled.div`
-
+  margin: 4rem 8rem;
+  display: flex;
+  justify-content: space-between;
 `
 export const CompletePedido = styled.div`
 
 `
 export const EnderecoContainer = styled.div`
+  margin-top: 1rem;
 
+  padding: 2.5rem;
+  width: 40rem;
+  height: 23rem;
+
+  border-radius: 6px;
+
+  background-color: ${({theme}) => theme.colors["base-card"]};
+  div {
+    display: flex;
+  }
 `
+
+export const HeaderContainerCards = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  div {
+    flex-direction: column;
+    gap: 0.3rem;
+
+    p {
+      color: ${({theme}) => theme.colors["base-subtitle"]};
+      font-size: ${({theme}) => theme.textSizes["text-regular-m"]};
+    }
+
+    span {
+      color: ${({theme}) => theme.colors["base-text"]};
+      font-size: ${({theme}) => theme.textSizes["text-regular-s"]};
+    }
+  }
+`
+
 export const PagamentoContainer = styled.div`
 
 `
