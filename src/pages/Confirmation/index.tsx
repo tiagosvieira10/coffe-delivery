4f51eff20981c2e1a325f9638e68db26d9ebb80b
Tiago Vieira <tiagosvieira10@gmail.com>
2023-09-27 15:16:14 -0300

    pedido confirmado finalizada

--- src/pages/Confirmation/index.tsx
+++ src/pages/Confirmation/index.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../../components/Header"
 import imgEntrega from '../../assets/Illustration.svg'
-import { ConfimationMain, ConfirmationContainer, DescriptionConfirmation, ImgdeEntrega, PedidoDescriptionConfirmation } from "./styles"
+import { ConfimationMain, ConfirmationContainer, ContainerEntrega, DescriptionConfirmation, ImgdeEntrega, PedidoDescriptionConfirmation } from "./styles"
 import { localizacaoEntrega, pagamentoEntrega, previsaoEntrega } from "../../assets/infoConfimation"
 
 export function ConfirmationPage() {
@@ -11,29 +11,32 @@ export function ConfirmationPage() {
         <DescriptionConfirmation>
           <h2>Uhu! Pedido confirmado</h2>
           <p>Agora é só aguardar que logo o café chegará até você</p>
-          <PedidoDescriptionConfirmation>
-            <img src={localizacaoEntrega} alt="" />
-            <div className="PedidoDescription">
-              <span>Entrega em <strong>Avenida Tamboril, 269</strong></span>
-              <span>Renascença - Ouricuri, PE</span>
-            </div>
-          </PedidoDescriptionConfirmation>
+        
+          <ContainerEntrega>
+            <PedidoDescriptionConfirmation>
+              <img src={localizacaoEntrega} alt="" />
+              <div className="PedidoDescription">
+                <span>Entrega em <strong>Avenida Tamboril, 269</strong></span>
+                <span>Renascença - Ouricuri, PE</span>
+              </div>
+            </PedidoDescriptionConfirmation>
 
-          <PedidoDescriptionConfirmation>
-            <img src={previsaoEntrega} alt="" />
-            <div className="PedidoDescription">
-              <span>Previsão de entrega</span>
-              <strong>20 min - 30 min</strong>
-            </div>
-          </PedidoDescriptionConfirmation>
+            <PedidoDescriptionConfirmation>
+              <img src={previsaoEntrega} alt="" />
+              <div className="PedidoDescription">
+                <span>Previsão de entrega</span>
+                <strong>20 min - 30 min</strong>
+              </div>
+            </PedidoDescriptionConfirmation>
 
-          <PedidoDescriptionConfirmation>
-            <img src={pagamentoEntrega} alt="" />
-            <div className="PedidoDescription">
-              <span>Pagamento na entrega</span>
-              <strong>Cartão de Crédito</strong>
-            </div>
-          </PedidoDescriptionConfirmation>
+            <PedidoDescriptionConfirmation>
+              <img src={pagamentoEntrega} alt="" />
+              <div className="PedidoDescription">
+                <span>Pagamento na entrega</span>
+                <strong>Cartão de Crédito</strong>
+              </div>
+            </PedidoDescriptionConfirmation>
+          </ContainerEntrega>
         </DescriptionConfirmation>
 
         <ImgdeEntrega>
--- src/pages/Confirmation/styles.ts
+++ src/pages/Confirmation/styles.ts
@@ -4,16 +4,46 @@ export const ConfirmationContainer = styled.div`
 
 ` 
 export const ConfimationMain = styled.div`
-  margin: 4rem 8rem;
+  margin: 5rem 8rem;
   display: flex;
+  justify-content: space-between;
+`
+export const ContainerEntrega = styled.div`
+  border: 1px solid;
+  border-radius: 6px 36px 6px 36px;
+  /* border-image: linear-gradient(to right, #DBAC2C, #8047F8);
+  border-image-slice: 1; */
+  padding: 2.5rem;  
 `
 
 export const DescriptionConfirmation = styled.div`
-
+  h2 {
+    font-family: ${({theme}) => theme.fonts.title};
+    font-size: ${({theme}) => theme.textSizes["title-title-l"]};
+    color: ${({theme}) => theme.colors["brand-yellow-dark"]};
+    /* margin-bottom: 0.5rem; */
+  }
+  p {
+    font-size: ${({theme}) => theme.textSizes["text-regular-l"]};
+    color: ${({theme}) => theme.colors["base-subtitle"]};
+    margin-bottom: 2rem;
+  }
 ` 
 export const PedidoDescriptionConfirmation = styled.div`
+  display: flex;
+  gap: 0.75rem;
+  line-height: 1.5;
 
+  &:nth-child(1),
+  &:nth-child(2) {
+    margin-bottom: 2rem;
+  }
+
+  div.PedidoDescription {
+    display: flex;
+    flex-direction: column;
+  }
 ` 
 export const ImgdeEntrega = styled.div`
-
+  margin-top: 7rem;
 ` 
