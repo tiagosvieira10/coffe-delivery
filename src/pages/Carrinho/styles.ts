c26f2c225b297713690368df2fc0349403651008
Tiago Vieira <tiagosvieira10@gmail.com>
2023-09-27 10:15:59 -0300

    carrinho em nova branch

--- src/pages/Carrinho/styles.ts
+++ src/pages/Carrinho/styles.ts
@@ -10,7 +10,7 @@ export const CarrinhoMain = styled.div`
   gap: 1.5rem;
 
   h4 {
-    font-family:  ${({theme}) => theme.textSizes["title-title-xs"]};
+    font-family:  ${({theme}) => theme.fonts.title};
     font-size: ${({theme}) => theme.textSizes["title-title-xs"]};
     color: ${({theme}) => theme.colors["base-subtitle"]};
   }
@@ -147,15 +147,15 @@ export const CoffeSelect = styled.div`
 `
 export const ResumoPedido = styled.div`
   width: 28rem;
-  height: 32rem;
   margin-top: 1rem;
   padding: 2.5rem;
 
   background-color: ${({theme}) => theme.colors["base-card"]};
   border-radius: 6px 44px 6px 44px;
 
-  button {
+  button.confirmarPedido {
     border: none;
+    margin-top: 1rem;
     background-color: ${({theme}) => theme.colors["brand-yellow"]};
     color: ${({theme}) => theme.colors["base-white"]};
     font-size: ${({theme}) => theme.textSizes["components-button-g"]};
@@ -167,4 +167,77 @@ export const ResumoPedido = styled.div`
     width: 23rem;
   }
 
+`
+
+export const ResumoCoffeSelect = styled.div`
+  display: flex;
+
+  justify-content: space-between;
+  border-bottom: 1px solid ${({theme}) => theme.colors["base-button"]};
+  margin-bottom: 2rem;
+  padding-top: 0.5rem;
+  
+  
+
+  
+  img {
+    width: 4rem;
+    height: 4rem;
+  }
+
+  h5 {
+    font-size: ${({theme}) => theme.textSizes["text-bold-m"]};
+    color: ${({theme}) => theme.colors["base-text"]};
+  }
+`
+export const CoffeSelectContainer = styled.div`
+  display: flex;
+  margin-bottom: 1.5rem;
+`
+export const DescriptionCoffeSelected = styled.div`
+  flex-direction: column;
+  margin-left: 1rem;
+  
+  span {
+    font-size: ${({theme}) => theme.textSizes["text-regular-m"]};
+    color: ${({theme}) => theme.colors["base-subtitle"]};
+  }
+`
+export const ActionForCoffeSelected = styled.div`
+  display: flex;
+  gap: 0.25rem;
+  margin-top: 0.5rem;
+
+  button {
+    padding: 0.5rem;
+    gap: 0.25rem;
+    display: flex;
+    align-items: center;
+    border: none;
+    border-radius: 6px;
+    background-color: ${({theme}) => theme.colors["base-button"]};
+
+    font-size: ${({theme}) => theme.textSizes["components-button-s"]};
+    color: ${({theme}) => theme.colors["base-text"]};
+  }
+`
+export const ResumoValueSelect = styled.div`
+
+  p {
+    font-size: ${({theme}) => theme.textSizes["text-regular-s"]};
+    color: ${({theme}) => theme.colors["base-text"]};
+  }
+
+  h4 {
+    font-family: ${({theme}) => theme.fonts.regular};
+    font-size: ${({theme}) => theme.textSizes["text-bold-l"]};
+    color: ${({theme}) => theme.colors["base-subtitle"]};
+  }
+  
+  div {
+    display: flex;
+    justify-content: space-between ;
+
+    margin: 1rem 0;
+  }
 `
