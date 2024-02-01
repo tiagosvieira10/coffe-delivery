8c46ca0c0a6e648481ee3c457818fe9cddbf53e3
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-02-01 11:12:44 -0300

    add coffe to cart adicionado a context

--- src/contexts/CartContexts.tsx
+++ src/contexts/CartContexts.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useState } from "react";
 import { Coffee } from "../pages/Home/components/CoffeCard";
+import { produce } from 'immer'
 
 export interface CartItem extends Coffee {
   quantity: number;
@@ -7,6 +8,7 @@ export interface CartItem extends Coffee {
 
 interface CartContentType {
   cartItems: CartItem[];
+  addCoffeeToCart: (coffee: CartItem) => void;
 }
 
 interface CartContextProviderProps {
@@ -17,8 +19,25 @@ export const CartContext = createContext({} as CartContentType)
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
   const [ cartItems, setCartItems ] = useState<CartItem[]>([]);
+
+  function addCoffeeToCart(coffee: CartItem) {
+    const coffeeAlReadyExistInCart = cartItems.findIndex(
+      (cartItem) => cartItem.id === coffee.id
+    );
+
+    const newCart = produce(cartItems, (draft) => {
+      if(coffeeAlReadyExistInCart < 0) {
+        draft.push(coffee);
+      } else {
+        draft[coffeeAlReadyExistInCart]. quantity += coffee.quantity
+      }
+    });
+
+    setCartItems(newCart)
+  }
+
   return (
-    <CartContext.Provider value={{ cartItems }}>
+    <CartContext.Provider value={{ cartItems, addCoffeeToCart }}>
       {children}
     </CartContext.Provider>
   )
